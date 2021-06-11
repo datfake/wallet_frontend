641aa859a21ab1243302a0b003d5daee6ab4e485
datngo <datnt@saltlux.com>
2021-06-11 09:22:48 +0700

    crud networks

--- src/components/NetWork.vue
+++ src/components/NetWork.vue
@@ -7,8 +7,11 @@
         <v-tab>Native</v-tab>
         <v-tab>Euro</v-tab>
       </v-tabs>
-      <v-app v-if="tab==0">
-        <v-row class="d-flex justify-end mb-6" style="    margin-top: 0px;margin-right: 15px;">
+      <v-app v-if="tab == 0">
+        <v-row
+          class="d-flex justify-end mb-6"
+          style="margin-top: 0px; margin-right: 15px; max-height: 0px"
+        >
           <v-dialog v-model="dialog" persistent max-width="600px">
             <template v-slot:activator="{ on, attrs }">
               <v-btn color="primary" dark v-bind="attrs" v-on="on">ADD</v-btn>
@@ -21,21 +24,37 @@
                 <v-container>
                   <v-row>
                     <v-col cols="12">
-                      <v-text-field label="Name*" required></v-text-field>
+                      <v-text-field
+                        v-model="chainName"
+                        label="Name*"
+                        required
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12">
-                      <v-text-field label="IP*" required></v-text-field>
+                      <v-text-field
+                        v-model="chainID"
+                        label="IP*"
+                        required
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12">
-                      <v-text-field label="RPC*" required></v-text-field>
+                      <v-text-field
+                        v-model="chainRPC"
+                        label="RPC*"
+                        required
+                      ></v-text-field>
                     </v-col>
                   </v-row>
                 </v-container>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-                <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+                <v-btn color="blue darken-1" text @click="dialog = false"
+                  >Close</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="addNetwork"
+                  >Save</v-btn
+                >
               </v-card-actions>
             </v-card>
           </v-dialog>
@@ -51,24 +70,25 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="item in desserts" :key="item.name">
+              <tr v-for="item in networks" :key="item.id">
                 <td>{{ item.name }}</td>
-                <td>{{ item.calories }}</td>
-                <td>{{ item.calories }}</td>
+                <td>{{ item.ip }}</td>
+                <td>{{ item.RPC }}</td>
                 <td>
-                  <v-btn depressed color="error">Delete</v-btn>
+                  <v-btn @click="deleteNetwork(item.id)" depressed color="error">Delete</v-btn>
                 </td>
               </tr>
             </tbody>
           </template>
         </v-simple-table>
       </v-app>
-      <v-app v-if="tab==1">he</v-app>
+      <v-app v-if="tab == 1">he</v-app>
     </v-card>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "NetWork",
 
@@ -76,49 +96,30 @@ export default {
     return {
       tab: 0,
       dialog: false,
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237
-        },
-        {
-          name: "Eclair",
-          calories: 262
-        },
-        {
-          name: "Cupcake",
-          calories: 305
-        },
-        {
-          name: "Gingerbread",
-          calories: 356
-        },
-        {
-          name: "Jelly bean",
-          calories: 375
-        },
-        {
-          name: "Lollipop",
-          calories: 392
-        },
-        {
-          name: "Honeycomb",
-          calories: 408
-        },
-        {
-          name: "Donut",
-          calories: 452
-        },
-        {
-          name: "KitKat",
-          calories: 518
-        }
-      ]
+      networks: null,
+      chainID: null,
+      chainName: null,
+      chainRPC: null,
     };
-  }
+  },
+  created() {
+    axios
+      .get("http://localhost:3300/wallet/network/list")
+      .then((response) => (this.networks = response.data));
+  },
+  methods: {
+    addNetwork() {
+      this.dialog = false;
+      axios
+        .post("http://localhost:3300/wallet/network/create", {
+          chainID: this.chainID,
+          chainName: this.chainName,
+           chainRPC: this.chainRPC,
+        })
+        .then(function (response) {
+          console.log(response);
+        });
+    },
+  },
 };
 </script>
